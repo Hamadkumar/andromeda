6f157a6718f8276531d84ede8c88114a356a78c6
Hamadkumar K <hamadkarthi@gmail.com>
2020-02-22 20:15:07 +0530

    #refs Unused header

--- src/main/java/com/io/andromeda/controller/AndromedaRestController.java
+++ src/main/java/com/io/andromeda/controller/AndromedaRestController.java
@@ -1,8 +1,7 @@
 package com.io.andromeda.controller;
 
-import java.util.List;
+
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpSession;
 
@@ -13,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
